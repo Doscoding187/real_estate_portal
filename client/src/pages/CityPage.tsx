2956599a25b75fc9b232b75254a7b9741b1588cf
Doscoding187 <enetechsa@gmail.com>
2025-12-15 18:44:04 +0200

    fix(city-page): remove duplicate topLocalities declaration

--- client/src/pages/CityPage.tsx
+++ client/src/pages/CityPage.tsx
@@ -78,7 +78,7 @@ export default function CityPage({ params }: { params: { province: string; city:
     topDevelopers = [],
     investmentProjects = [],
     recommendedAgencies = [],
-    topLocalities = []
+
   } = data || {};
 
   // Additional validation - city must exist
